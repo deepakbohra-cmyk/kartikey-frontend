74d85f2fb783579fa2eff12f1509e418c412825b
deepakbohra-cmyk <deepak.bohra@vacobinary.in>
2025-10-09 15:14:21 +0530

    Revert "6oct changes"

--- src/api/l1TeamAPI.js
+++
@@ -1,8 +0,0 @@
-import { apiClient, API_ENDPOINTS } from "./config";
-
-export const l1TeamAPI = {
-  createForm: async (formData) => {
-    const response = await apiClient.post(API_ENDPOINTS.L1TEAM.CREATE, formData);
-    return response.data;
-  },
-};
