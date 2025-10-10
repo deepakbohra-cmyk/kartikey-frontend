c3d12edfd4b3e5e607a96904cb412a6bbc1d4524
Deepak <deepak.bohra@vacobinary.in>
2025-10-10 15:37:05 +0530

    done

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_CONFIG = {
-  BASE_URL: "http://localhost:8080/api",
+  BASE_URL: import.meta.env.BASEURL,
   TIMEOUT: 10000,
   HEADERS: {
     "Content-Type": "application/json",
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
   };
 
   const startGoogleOAuth = () => {
-    window.location.href = "http://localhost:8080/oauth2/authorization/google";
+    window.location.href = `${import.meta.env.BASEURL}/oauth2/authorization/google`;
   };
 
   return (
